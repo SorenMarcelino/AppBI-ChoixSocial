c4f5a41d5e2e9b1fecc932b2c32842dadd8bd050
Théo Quezel-Perron <explo8413@gmail.com>
2024-03-11 09:59:49 +0100

    finish work on stats

---
+++ Statistiques.py
@@ -0,0 +1,71 @@
+import matplotlib.pyplot as plt
+
+
+def size_of_dataset(data):
+    row = len(data)
+    columns = len(data.columns)
+    return [row, columns]
+
+
+def is_classement_uniq(data):
+    codex = []
+    occurence = []
+    for column in data.columns[0:]:
+        classement = ""
+        for val in data[column]:
+            classement += val
+        if classement not in codex:
+            codex.append(classement)
+            occurence.append(1)
+        else:
+            i = codex.index(classement)
+            occurence[i] += 1
+
+    return [codex, occurence]
+
+
+def frequency_per_ranks_per_crit(data):
+    dim = size_of_dataset(data)
+    crits = is_classement_uniq(data)
+    rslt = {}
+    id = 0
+    for x in crits[0]:
+        if id == 0:
+            for y in x:
+                rslt[y] = []
+                for z in range(0, len(x)):
+                    rslt[y].append(0)
+
+        index = 0
+        for y in x:
+            rslt[y][index] += 1 * crits[1][id]
+            index += 1
+
+        id += 1
+
+    return rslt
+
+
+def plot_frequency(data):
+    pos = 0
+    position = []
+
+    for x in data.keys():
+        position.append(pos)
+        pos += 1
+
+    print(position)
+
+    for x, y in data.items():
+        print(x,y)
+        plt.plot(position, y, label=x)
+
+    plt.legend()
+    plt.show()
+
+    fig, ax = plt.subplots()
+    for x, y in data.items():
+        ax.bar(position, y, width=1, edgecolor="white", linewidth=0.7)
+        plt.legend = x
+        plt.show()
+        fig, ax = plt.subplots()
--- main.py
+++ main.py
@@ -1,3 +1,17 @@
 import pandas as pd
+from Statistiques import *
 
-data_vote = pd.read_csv(filepath_or_buffer='data/profil2.csv', delimiter=',', )
+data_vote = pd.read_csv(filepath_or_buffer='data/exo1.csv', delimiter=',', header=None)
+
+# Get size of dataset
+print(data_vote)
+dim = size_of_dataset(data_vote)
+print(dim)
+print("Le dataset contient " + str(dim[1]) + " classement(s) de " + str(dim[0]) + " critères")
+
+uniq_ranks = is_classement_uniq(data_vote)
+print(uniq_ranks)
+
+frq_val = frequency_per_ranks_per_crit(data_vote)
+
+plot_frequency(frq_val)
