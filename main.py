11f2bc13ec8636c92e33fa01f5704aed0e7037e7
Théo Quezel-Perron <explo8413@gmail.com>
2024-03-16 21:13:33 +0100

    Vote alterntif

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 from voteUnTour import *
 from borda import *
+from alternative import *
 import pandas as pd
 from Statistiques import *
 
-data_vote = pd.read_csv(filepath_or_buffer='data/profil3.csv    ', delimiter=',', header=None)
+data_vote = pd.read_csv(filepath_or_buffer='data/exo1_bis.csv', delimiter=',', header=None)
 
 '''
 STAT
@@ -15,12 +16,12 @@ dim = size_of_dataset(data_vote)
 print(dim)
 print("Le dataset contient " + str(dim[1]) + " classement(s) de " + str(dim[0]) + " critères")
 
-uniq_ranks = is_classement_uniq(data_vote)
-print(uniq_ranks)
+#uniq_ranks = is_classement_uniq(data_vote)
+#print(uniq_ranks)
 
-frq_val = frequency_per_ranks_per_crit(data_vote)
+#frq_val = frequency_per_ranks_per_crit(data_vote)
 
-plot_frequency(frq_val)
+#plot_frequency(frq_val)
 
 '''
 Fin STAT
@@ -30,7 +31,7 @@ Fin STAT
 Vote à X tour(s)
 '''
 
-voteUnTour(data_vote)
+#voteUnTour(data_vote)
 
 '''
 Vote à X tour(s)
@@ -40,8 +41,17 @@ Vote à X tour(s)
 '''
 Borda
 '''
-classementBorda(methodeBorda(frq_val))
-print(vainqueurBorda(methodeBorda(frq_val)))
+#classementBorda(methodeBorda(frq_val))
+#print(vainqueurBorda(methodeBorda(frq_val)))
 '''
 Fin Borda
+'''
+
+'''
+Alternatif
+'''
+print(vote_alternatif(data_vote, dim[1]))
+print(vote_alternatif_classement(data_vote))
+'''
+Fin Alternatif
 '''
